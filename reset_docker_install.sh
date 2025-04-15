7f66aa900f2c31b6772daedf0a6f75a8a7b24b8e
apos <apos.pospischil@quanto-solutions.de>
2025-04-15 18:06:39 +0200

    Fix: Config-Datei (DB, Port, etc.) wird in 01_container_bauen.sh und 02_Wordpress_installieren.sh geladen

--- reset_docker_install.sh
+++ reset_docker_install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 
 echo "🧹 Setze WordPress-Docker-Installation zurück..."
 docker compose down --volumes
 docker volume prune -f
 sudo rm -rf ./wp_data
-echo "✅ Zurückgesetzt. Starte nun setup_wordpress_docker.sh erneut."
+echo "✅ Zurückgesetzt. Starte nun 01_container_bauen.sh erneut."
